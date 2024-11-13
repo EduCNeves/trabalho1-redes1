d5b28d8f5f1eab5a53e8a74f85da5426184dc128
Arthur Castro <arthurcastro078@gmail.com>
2024-11-13 16:05:03 -0300

    teste

--- client/client.c
+++ client/client.c
@@ -4,11 +4,13 @@
 #include "commands.h"
 #include "util.h"
 
-int main() {
+int main()
+{
     int choice;
-    char file[256];
+    char *file;
 
-    while (1) {
+    while (1)
+    {
         // Exibe o menu de opções
         printf("\nMenu:\n");
         printf("1. Backup\n");
@@ -17,47 +19,65 @@ int main() {
         printf("4. List files in backup directory\n");
         printf("5. Exit\n");
         printf("Escolha sua tarefa 1-5): ");
-        scanf("%d", &choice);
-        getchar(); // Para consumir o caractere de nova linha após scanf
 
-        switch (choice) {
-            case 1: // Backup
-                printf("Digite o nome de arquivo para backup: ");
-                fgets(file, sizeof(file), stdin);
-                file[strcspn(file, "\n")] = 0; // Remove newline character
-                while (!validate_file(file)) {
-                    printf("Erro: o arquivo não existe no diretorio atual.\n");
-                    printf("Digite um nome valido de arquivo para backup: ");
-                    fgets(file, sizeof(file), stdin);
-                    file[strcspn(file, "\n")] = 0; // Remove newline character
-                }
-                backup(file);
-                break;
+        if (scanf("%d", &choice) != 1)
+        {
+            choice = -1; // Define choice como inválido se a entrada não for um número
+        }
 
-            case 2: // Restore
-                printf("Digite o nome de arquivo para restaurar: ");
-                fgets(file, sizeof(file), stdin);
-                file[strcspn(file, "\n")] = 0; // Remove newline character
-                restore(file);
-                break;
+        clear_input_buffer(); // Limpa o buffer após uma entrada válida ou inválida
 
-            case 3: // Check
-                printf("Digite o nome de arquivo para verificar: ");
-                fgets(file, sizeof(file), stdin);
-                file[strcspn(file, "\n")] = 0; // Remove newline character
-                check(file);
-                break;
+        CommandError e;
 
-            case 4: // List files
-                list_files();
-                break;
+        switch (choice)
+        {
+        case 1: // Backup
+            file = get_valid_filename("Digite o nome de arquivo para backup: ");
 
-            case 5: // Exit
-                printf("Saindo do programa...\n");
-                return 0;
+            e = backup(file);
+            if (e != COMMAND_SUCCESS)
+            {
+                handle_command_error(e);
+            }
 
-            default:
-                printf("Escolha invalida. Escolha um numero entre 1 e 4.\n");
+            free(file);
+            break;
+
+        case 2: // Restore
+            char f[256];
+            printf("Digite o nome de arquivo para restaurar: ");
+            fgets(f, sizeof(f), stdin);
+            f[strcspn(f, "\n")] = 0; // Remove newline character
+
+            e = restore(f);
+            if (e != COMMAND_SUCCESS)
+            {
+                handle_command_error(e);
+            }
+            break;
+
+        case 3: // Check
+            file = get_valid_filename("Digite o nome de arquivo para verificar: ");
+
+            e = check(file);
+            if (e != COMMAND_SUCCESS)
+            {
+                handle_command_error(e);
+            }
+
+            free(file);
+            break;
+
+        case 4: // List files
+            list_files();
+            break;
+
+        case 5: // Exit
+            printf("Saindo do programa...\n");
+            return 0;
+
+        default:
+            printf("Escolha invalida. Escolha um numero entre 1 e 4.\n");
         }
     }
 
